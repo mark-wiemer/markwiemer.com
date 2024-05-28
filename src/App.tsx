92d0b5f36932a43368a079cbd5c96b7641a1aca9
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-05-27 19:55:29 -0700

    Add error page for bad routes (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,24 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>markwiemer.com</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+          }}
+        >
           Count is {count}
         </button>
         <p>
+          {/* Wrap as string to avoid VS Code yelling at raw HTML `)` */}
           {`Hello, world! If you're reading this, it's because you're a cool
           person. There isn't much here yet, just wanted to show you my little
           corner of the internet. I'll let you know when there's more to see :)`}
@@ -35,7 +40,7 @@ function App() {
           Built with <a href="https://bun.sh">Bun</a>
         </p>
         <p>
-          "Smiling face with sunglasses" graphic designed by{" "}
+          {`"Smiling face with sunglasses" graphic designed by `}
           <a href="https://openmoji.org">OpenMoji</a> – the open-source emoji
           and icon project.{" "}
           <a href="https://creativecommons.org/licenses/by-sa/4.0">
