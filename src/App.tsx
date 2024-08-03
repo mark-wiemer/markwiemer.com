d80fb7dcceb6b4a1cb16c5f0ac89b88ec5406d72
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-08-03 12:34:14 -0700

    Add homepage, links to about page, custom styles (#17)

---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import {
+  BrandVariants,
+  createDarkTheme,
+  FluentProvider,
+  makeStyles,
+  Theme,
+} from "@fluentui/react-components";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import About from "./routes/about/About";
+import Breakout from "./routes/games/breakout/Breakout";
+import Home from "./routes/home/Home";
+import { StrictMode } from "react";
+
+const useAppStyles = makeStyles({
+  provider: {
+    minWidth: "320px",
+    maxWidth: "100vw",
+    "& div": {
+      display: "flex",
+      flexDirection: "column",
+    },
+    fontSize: "18px",
+    lineHeight: "1.5",
+    "& h1": {
+      fontSize: "3em",
+    },
+  },
+});
+
+// https://react.fluentui.dev/?path=/docs/theme-theme-designer--page
+// 2024-08-03
+// Key color value: #A016BB
+// Hue torsion: -31
+// Vibrancy: 12
+const plumBrandVariants: BrandVariants = {
+  10: "#060105",
+  20: "#270D24",
+  30: "#430E40",
+  40: "#580C59",
+  50: "#6E0873",
+  60: "#82058F",
+  70: "#9608AB",
+  80: "#A422C1",
+  90: "#AC3FCD",
+  100: "#B457D7",
+  110: "#BB6DDF",
+  120: "#C382E7",
+  130: "#CB96ED",
+  140: "#D3A9F2",
+  150: "#DBBCF7",
+  160: "#E5CFFA",
+};
+
+const plumDarkTheme: Theme = {
+  ...createDarkTheme(plumBrandVariants),
+};
+
+plumDarkTheme.colorBrandForeground1 = plumBrandVariants[110];
+plumDarkTheme.colorBrandForeground2 = plumBrandVariants[120];
+
+const App = () => {
+  const styles = useAppStyles();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/games/breakout",
+      element: <Breakout />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+
+  return (
+    <StrictMode>
+      <FluentProvider theme={plumDarkTheme} className={styles.provider}>
+        <RouterProvider router={router} />
+      </FluentProvider>
+    </StrictMode>
+  );
+};
+
+export default App;
---
+++ src/routes/home/Home.tsx
@@ -0,0 +1,91 @@
+import { useHomeStyles } from "./Home.styles";
+import { Link, LargeTitle } from "@fluentui/react-components";
+import profile2023 from "../../assets/2023-07-20 profile.png";
+import wynonnaRaincoat from "../../assets/2024-04-29 Wynonna rain coat outside cropped.jpg";
+import { useNavigate } from "react-router-dom";
+
+const Home = (): JSX.Element => {
+  const styles = useHomeStyles();
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <LargeTitle as="h1" style={{ textAlign: "center" }}>
+        mark
+        <wbr />
+        wiemer
+        <wbr />
+        .com
+      </LargeTitle>
+      <div
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          columnGap: "1em",
+          rowGap: "1em",
+          alignSelf: "center",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        <img src={profile2023} className={styles.headerImage} />
+        <img src={wynonnaRaincoat} className={styles.headerImage} />
+      </div>
+      <div className={styles.card}>
+        <p>
+          {/* Wrap as string to avoid VS Code yelling at raw HTML `)` */}
+          {`Hello, world! If you're reading this, it's because you're a cool
+          person. There isn't much here yet, just wanted to show you my little
+          corner of the internet. I'll let you know when there's more to see :)`}
+        </p>
+        <p>
+          {`(P.S. I'm the one on the left. The cutie on the right is Wynonna, 
+          my Australian shepherd 🥰)`}
+        </p>
+        <p>
+          {`"Smiling face with sunglasses" graphic designed by `}
+          <Link href="https://openmoji.org" target="_blank" rel="noreferrer">
+            OpenMoji
+          </Link>{" "}
+          – the open-source emoji and icon project.{" "}
+          <Link
+            href="https://creativecommons.org/licenses/by-sa/4.0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            License: CC BY-SA 4.0
+          </Link>
+        </p>
+        <div
+          style={{
+            flexDirection: "row",
+            columnGap: "1em",
+            rowGap: "1em",
+            alignSelf: "center",
+          }}
+        >
+          <Link
+            as="a"
+            onClick={() => {
+              navigate("/about");
+            }}
+            style={{ fontSize: "18px" }}
+          >
+            About me
+          </Link>
+          |
+          <Link
+            href="https://github.com/mark-wiemer/mark-wiemer-com"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textAlign: "center" }}
+          >
+            View on GitHub
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
