4dc5956ce04ebb073d8864f0405c59b096ae0311
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-08-03 12:34:14 -0700

    Add homepage, links to about page, custom styles (#17)

---
+++ src/routes/home/Home.styles.ts
@@ -0,0 +1,18 @@
+import { makeStyles } from "@fluentui/react-components";
+
+export const useHomeStyles = makeStyles({
+  container: {
+    maxWidth: "1280px",
+    padding: "2rem",
+    textAlign: "center",
+  },
+  card: {
+    padding: "2em",
+  },
+  headerImage: {
+    width: "12em",
+    height: "12em",
+    alignSelf: "center",
+    borderRadius: "50%",
+  },
+});
