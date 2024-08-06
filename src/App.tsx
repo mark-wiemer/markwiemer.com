6755353864501fc3c79fd54b5613d514c781db42
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-08-06 00:56:23 -0700

    Make theme accessible (#23)

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,11 @@
-import {
-  BrandVariants,
-  createDarkTheme,
-  FluentProvider,
-  Theme,
-} from "@fluentui/react-components";
+import { FluentProvider, webDarkTheme } from "@fluentui/react-components";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
 import About from "./routes/about/About";
 import Breakout from "./routes/games/breakout/Breakout";
 import Home from "./routes/home/Home";
 import { StrictMode } from "react";
-import useAppStyles from "./App.styles";
-
-// https://react.fluentui.dev/?path=/docs/theme-theme-designer--page
-// 2024-08-03
-// Key color value: #A016BB
-// Hue torsion: -31
-// Vibrancy: 12
-const plumBrandVariants: BrandVariants = {
-  10: "#060105",
-  20: "#270D24",
-  30: "#430E40",
-  40: "#580C59",
-  50: "#6E0873",
-  60: "#82058F",
-  70: "#9608AB",
-  80: "#A422C1",
-  90: "#AC3FCD",
-  100: "#B457D7",
-  110: "#BB6DDF",
-  120: "#C382E7",
-  130: "#CB96ED",
-  140: "#D3A9F2",
-  150: "#DBBCF7",
-  160: "#E5CFFA",
-};
-
-const plumDarkTheme: Theme = {
-  ...createDarkTheme(plumBrandVariants),
-};
-
-plumDarkTheme.colorBrandForeground1 = plumBrandVariants[110];
-plumDarkTheme.colorBrandForeground2 = plumBrandVariants[120];
+import useAppStyles, { brandThemeColors } from "./App.styles";
 
 const App = () => {
   const styles = useAppStyles();
@@ -66,7 +30,10 @@ const App = () => {
 
   return (
     <StrictMode>
-      <FluentProvider theme={plumDarkTheme} className={styles.provider}>
+      <FluentProvider
+        theme={{ ...webDarkTheme, ...brandThemeColors }}
+        className={styles.provider}
+      >
         <RouterProvider router={router} />
       </FluentProvider>
     </StrictMode>
