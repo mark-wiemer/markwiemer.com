25e3f6d798debd04b863b17656f3c6f89a95a9b2
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-08-06 20:17:23 -0700

    Add light theme support (#27)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,66 @@
-import { FluentProvider, webDarkTheme } from "@fluentui/react-components";
+import {
+  createDarkTheme,
+  createLightTheme,
+  FluentProvider,
+  webDarkTheme,
+  webLightTheme,
+} from "@fluentui/react-components";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
 import About from "./routes/about/About";
 import Breakout from "./routes/games/breakout/Breakout";
 import Home from "./routes/home/Home";
-import { StrictMode } from "react";
-import useAppStyles, { brandThemeColors } from "./App.styles";
+import { StrictMode, useEffect, useMemo, useState } from "react";
+import useAppStyles, {
+  brandDarkThemeColors,
+  brandLightThemeColors,
+  brandVariants,
+} from "./App.styles";
 
 const App = () => {
   const styles = useAppStyles();
+  const [useDarkTheme, setUseDarkTheme] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+  const darkTheme = useMemo(
+    () => ({
+      ...webDarkTheme,
+      ...createDarkTheme(brandVariants),
+      ...brandDarkThemeColors,
+    }),
+    []
+  );
+  const lightTheme = useMemo(
+    () => ({
+      ...webLightTheme,
+      ...createLightTheme(brandVariants),
+      ...brandLightThemeColors,
+    }),
+    []
+  );
+
+  useEffect(() => {
+    document.body.style.backgroundColor = useDarkTheme ? "#1f1f1f" : "#ffffff";
+  }, [useDarkTheme]);
+
+  // Run on mount only: check for dark mode preference
+  useEffect(() => {
+    // Get current state
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+    if (prefersDarkMode.matches) setUseDarkTheme(true);
+    else setUseDarkTheme(false);
+
+    // Listen for future changes
+    prefersDarkMode.addEventListener("change", (e) => {
+      if (e.matches) setUseDarkTheme(true);
+      else setUseDarkTheme(false);
+    });
+    return () => {
+      prefersDarkMode.removeEventListener("change", () => {
+        /* no-op */
+      });
+    };
+  }, []);
 
   const router = createBrowserRouter([
     {
@@ -31,7 +83,7 @@ const App = () => {
   return (
     <StrictMode>
       <FluentProvider
-        theme={{ ...webDarkTheme, ...brandThemeColors }}
+        theme={useDarkTheme ? darkTheme : lightTheme}
         className={styles.provider}
       >
         <RouterProvider router={router} />
