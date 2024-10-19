382de26480e374a16ae15dd909275b410ee00f2e
Mark Wiemer <markwiemer@outlook.com>
2024-10-19 09:20:56 -0700

    Update ESLint to v9

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,4 @@ import App from "./App.tsx";
 import "./main.css";
 
 // `root` is guaranteed by `index.html` (if it's not there, we have bigger issues!)
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      enforce: "pre",
-      ...mdx(),
-    },
-    react(),
-  ],
+  plugins: [{ enforce: "pre", ...mdx() }, react()],
 });
