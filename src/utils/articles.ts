6a1516612d22a75feffaa9f3db2121fc2b78eb1f
Mark Wiemer <markwiemer@outlook.com>
2024-10-19 10:59:44 -0700

    Add months without music article

--- src/utils/articles.ts
+++ src/utils/articles.ts
@@ -1,12 +1,15 @@
 import ErrorPage from '../components/ErrorPage';
 import About from '../routes/blog/2023-04-17-who-am-i.mdx';
+import MonthsWithoutMusic from '../routes/blog/2024-10-08-months-without-music.mdx';
 
 export enum ArticleId {
     About = 'about',
+    MonthsWithoutMusic = 'months-without-music',
     NotFound = 'notFound',
 }
 
 export const articlesById: Record<ArticleId, React.FC> = {
     [ArticleId.About]: About,
+    [ArticleId.MonthsWithoutMusic]: MonthsWithoutMusic,
     [ArticleId.NotFound]: ErrorPage,
 };
