10aff22ee80e965e8501e0eacb9960194d45cc7e
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-12-21 22:23:17 -0600

    Fix CD (#57)

--- bundle.bash
+++ bundle.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # For now, the dist is the same as the site
 # But as JS and whatnot are added, this will do the needful :)
+#* Always triple-check end-to-end deployment after changing this script!
 mkdir -p dist
-cp -r site dist
+cp -r site/* dist
