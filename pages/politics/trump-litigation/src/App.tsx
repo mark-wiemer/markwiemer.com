8f3bc439e66ed3f77d1ff6f87efd614805165f96
Mark Wiemer <markwiemer@outlook.com>
2025-06-22 11:41:06 -0700

    Replace page with Fluent DataGrid

--- pages/politics/trump-litigation/src/App.tsx
+++ pages/politics/trump-litigation/src/App.tsx
@@ -1,14 +1,10 @@
-import { useCounter } from "./hooks/useCounter";
+import { Default } from "./DataGrid";
 
 function App() {
-    const { count, increment } = useCounter();
-
     return (
         <>
-            <h1>Hello Vite + React + TypeScript!</h1>
-            <button type="button" onClick={increment}>
-                Count is: {count}
-            </button>
+            <h1>Hello Fluent DataGrid!</h1>
+            <Default></Default>
         </>
     );
 }
