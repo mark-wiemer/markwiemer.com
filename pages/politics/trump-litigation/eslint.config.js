04b3240781cf1f2531078920256a62ad2362b9d4
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2025-06-22 11:13:49 -0700

    Update all dependencies for politics page (#88)

--- pages/politics/trump-litigation/eslint.config.js
+++ pages/politics/trump-litigation/eslint.config.js
@@ -4,5 +4,7 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-    ignores: ["dist"],
+    linterOptions: {
+        reportUnusedDisableDirectives: "error",
+    },
 });
