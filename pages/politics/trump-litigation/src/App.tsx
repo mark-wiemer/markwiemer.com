ce4a13a8e757bcd79b2b20a946fa962337a14c13
Mark Wiemer <markwiemer@outlook.com>
2025-06-22 11:20:06 -0700

    Remove unused politics page stuff

--- pages/politics/trump-litigation/src/App.tsx
+++ pages/politics/trump-litigation/src/App.tsx
@@ -6,42 +6,9 @@ function App() {
     return (
         <>
             <h1>Hello Vite + React + TypeScript!</h1>
-            <p>
-                <button type="button" onClick={increment}>
-                    Count is: {count}
-                </button>
-            </p>
-            <p>
-                Edit <code>App.tsx</code> and save to test HMR updates.
-            </p>
-            <p>
-                <a
-                    className="App-link"
-                    href="https://vite.dev"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Vite docs
-                </a>
-                {" | "}
-                <a
-                    className="App-link"
-                    href="https://react.dev"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    React docs
-                </a>
-                {" | "}
-                <a
-                    className="App-link"
-                    href="https://typescriptlang.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    TypeScript docs
-                </a>
-            </p>
+            <button type="button" onClick={increment}>
+                Count is: {count}
+            </button>
         </>
     );
 }
