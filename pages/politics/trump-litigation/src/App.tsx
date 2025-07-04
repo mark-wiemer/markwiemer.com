e2ffcaf99df61d3156950e2295f023961baae86f
Mark Wiemer <markwiemer@outlook.com>
2025-07-04 10:14:02 -0700

    Add external link and cite sources

--- pages/politics/trump-litigation/src/App.tsx
+++ pages/politics/trump-litigation/src/App.tsx
@@ -1,10 +1,22 @@
-import { Default } from "./DataGrid";
+import { TrumpLitigationDataGrid } from "./DataGrid";
+import { ExternalLink } from "./ExternalLink";
 
 function App() {
     return (
         <>
-            <h1>Hello Fluent DataGrid!</h1>
-            <Default></Default>
+            <h1>Trump Litigation Tracker</h1>
+            <p>
+                Building on the efforts of{" "}
+                <ExternalLink href="https://www.courtwatch.news/p/lawsuits-related-to-trump-admin-executive-orders">
+                    courtwatch.news
+                </ExternalLink>{" "}
+                and{" "}
+                <ExternalLink href="https://www.justsecurity.org/107087/tracker-litigation-legal-challenges-trump-administration">
+                    Just Security
+                </ExternalLink>
+                , this page tracks litigation against the Trump administration.
+            </p>
+            <TrumpLitigationDataGrid />
         </>
     );
 }
