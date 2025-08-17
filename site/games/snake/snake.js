2e263a3103b20372b67051a1ceb6babba53b4db7
Mark Wiemer <markwiemer@outlook.com>
2025-08-16 21:10:21 -0700

    Fill canvas white

--- site/games/snake/snake.js
+++ site/games/snake/snake.js
@@ -7,3 +7,6 @@ const canvasSize = Math.min(window.innerWidth, window.innerHeight) * 0.9;
 const cellSize = canvasSize / size;
 canvas.setAttribute("width", canvasSize);
 canvas.setAttribute("height", canvasSize);
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvasSize, canvasSize);
