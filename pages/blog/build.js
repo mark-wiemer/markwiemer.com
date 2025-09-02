18282a58a869bb494a8149344b35d39ec78fbcc0
Mark Wiemer <markwiemer@outlook.com>
2025-09-01 20:37:48 -0700

    Add article title to page title

--- pages/blog/build.js
+++ pages/blog/build.js
@@ -23,10 +23,17 @@ async function build() {
     console.log(`Building ${fileNames.length} ${fileNames.length === 1 ? "file" : "files"}`);
     const templatePath = path.join(blogDir, "template.html");
     const template = fs.readFileSync(templatePath, "utf8");
-    const replace = `<!-- __ARTICLE_CONTENTS__ -->`;
+    const contentPlaceholder = `<!-- __ARTICLE_CONTENTS__ -->`;
+    const titlePlaceholder = `<!-- __ARTICLE_TITLE__ -->`;
     for (const fileName of fileNames) {
+        console.log(`Building ${fileName}`);
         const filePath = path.join(blogDir, fileName);
         const mdString = fs.readFileSync(filePath, "utf8");
+        // Extract main heading from markdown (first line starting with # )
+        const headingMatch = mdString.match(/^#\s+(.+)$/m);
+        if (!headingMatch?.[1]) throw new Error(`Couldn't find main heading in "${fileName}"`);
+        const mainHeading = headingMatch[1].trim();
+        // Replace <title> in template
         const htmlBodyString = await remark()
             .use(remarkRehype, { allowDangerousHtml: true })
             .use(rehypeRaw)
@@ -35,9 +42,11 @@ async function build() {
             .process(mdString);
         const outName = fileName.replace(/\.md$/, ".html").slice("yyyy-mm-dd-".length);
         const outPath = path.join(siteDir, outName);
-        const fullHtml = template.replace(replace, htmlBodyString);
+        const fullHtml = template
+            .replace(contentPlaceholder, htmlBodyString)
+            .replace(titlePlaceholder, mainHeading);
         fs.writeFileSync(outPath, String(fullHtml));
-        console.log(`Wrote ${outPath}`);
+        console.log(`Wrote ${outName}`);
     }
     console.log(`All blog HTML written to site dir`);
 }
