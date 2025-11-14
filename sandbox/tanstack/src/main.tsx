2920960879fd73250c97aa7d5f273d8cc215daed
Mark Wiemer <markwiemer@outlook.com>
2025-11-13 20:18:46 -0800

    Add minimal Tanstack Table project

---
+++ sandbox/tanstack/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SimpleTable from "./SimpleTable.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <SimpleTable />
+    </React.StrictMode>,
+);
